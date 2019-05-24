f82fd5742fe4f45df8b893cb7d61d78eb5a86e2e
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-24 09:08:04 +0300

    Add working example of fetching data from the GitHub API

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,41 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, field, list, map3, string)
+
+
+type alias Member =
+    { login : String
+    , avatar_url : String
+    , html_url : String
+    }
+
+
+type alias Members =
+    List Member
 
 
 type alias Model =
-    { count : Int }
+    { org : String
+    , members : Members
+    , status : GithubRequestStatus
+    }
+
+
+type GithubRequestStatus
+    = NotRequested
+    | Loading
+    | Failure
+    | Success
 
 
 initialModel : Model
 initialModel =
-    { count = 0 }
+    { org = "andela", members = [], status = NotRequested }
 
 
 init : ( Model, Cmd Msg )
@@ -19,29 +43,82 @@ init =
     ( initialModel, Cmd.none )
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
 type Msg
-    = Increment
-    | Decrement
+    = RequestOrgUsers
+    | GotMembers (Result Http.Error Members)
+
+
+fetchOrgUsers : String -> Cmd Msg
+fetchOrgUsers org =
+    Http.get
+        { url = "https://api.github.com/orgs/" ++ org ++ "/members"
+        , expect = Http.expectJson GotMembers membersDecoder
+        }
+
+
+membersDecoder : Decoder Members
+membersDecoder =
+    list memberDecoder
+
+
+memberDecoder : Decoder Member
+memberDecoder =
+    map3 Member
+        (field "login" string)
+        (field "avatar_url" string)
+        (field "html_url" string)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | count = model.count + 1 }, Cmd.none )
+        RequestOrgUsers ->
+            ( { model | status = Loading }
+            , fetchOrgUsers model.org
+            )
 
-        Decrement ->
-            ( { model | count = model.count - 1 }, Cmd.none )
+        GotMembers result ->
+            case result of
+                Ok members ->
+                    ( { model | status = Success, members = members }, Cmd.none )
+
+                Err _ ->
+                    ( { model | status = Failure }, Cmd.none )
+
+
+tableRow : Member -> Html Msg
+tableRow member =
+    tr []
+        [ td []
+            [ img [ class "avatar", src member.avatar_url, alt (member.login ++ " avatar") ] []
+            ]
+        , td []
+            [ a [ href member.html_url, target "_blank" ] [ text member.login ]
+            ]
+        ]
 
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Elm app"
+    { title = "Elm GH Users app"
     , body =
-        [ div []
-            [ button [ onClick Increment ] [ text "+1" ]
-            , div [] [ text <| String.fromInt model.count ]
-            , button [ onClick Decrement ] [ text "-1" ]
+        [ div [ class "container" ]
+            [ h1 [] [ text "Hello from Elm GitHub Users App" ]
+            , button [ onClick RequestOrgUsers ] [ text "Request data" ]
+            , table [ class "u-full-width" ]
+                [ thead []
+                    [ tr []
+                        [ th [] [ text "Avatar" ]
+                        , th [] [ text "Name" ]
+                        ]
+                    ]
+                , tbody [] (List.map tableRow model.members)
+                ]
             ]
         ]
     }
@@ -53,5 +130,5 @@ main =
         { init = \_ -> init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
